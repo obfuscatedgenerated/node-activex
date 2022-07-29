f4f6f33cce78653740e1ac0fcff8ac5850f18ca5
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-07-29 20:21:15 +0100

    only include vscode electron node (no node-pre-gyp dynamic selection) as it doesnt play nice with webpack

--- activex.js
+++ activex.js
@@ -1,4 +1,4 @@
-var ActiveX = module.exports = require('node-gyp-build')(__dirname)
+var ActiveX = module.exports = require("./prebuilds/win32-x64/electron.abi103.node");
 
 global.ActiveXObject = function(id, opt) {
     return new ActiveX.Object(id, opt);
