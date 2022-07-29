b055e0dc8a9464cc3437f8ac22ff220a6cdbe82d
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-07-29 15:26:03 +0100

    node-gyp-build decides prebuild

--- activex.js
+++ activex.js
@@ -1,4 +1,4 @@
-var ActiveX = module.exports = require('./build/Release/node_activex.node');
+var ActiveX = module.exports = require('node-gyp-build')(__dirname)
 
 global.ActiveXObject = function(id, opt) {
     return new ActiveX.Object(id, opt);
