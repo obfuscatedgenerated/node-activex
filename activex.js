d72d7dde09ba8c09c3066f82b35d6547eaf2ab5a
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-09-20 19:53:22 +0100

    abi106

--- activex.js
+++ activex.js
@@ -1,4 +1,4 @@
-var ActiveX = module.exports = require("./prebuilds/win32-x64/electron.abi103.node");
+var ActiveX = module.exports = require("./prebuilds/win32-x64/electron.abi106.node");
 
 global.ActiveXObject = function(id, opt) {
     return new ActiveX.Object(id, opt);
